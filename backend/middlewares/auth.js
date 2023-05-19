3d2921bf326aadc3d160c532ab0c39675cf69c3d
Anastasia Talalaeva <LightTross@yandex.ru>
2023-05-19 19:40:02 +0300

    api

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,47 +3,21 @@ const jwt = require('jsonwebtoken');
 const { NODE_ENV, JWT_SECRET } = process.env;
 const { UnauthorizedError } = require('../errors/UnauthorizedError');
 
-// module.exports = (req, res, next) => {
-//   // извлекаем токен
-//   const token = req.cookies.jwt;
-
-//   let payload;
-
-//   try {
-//     // пытаемся верифицировать токен
-//     payload = jwt.verify(token, NODE_ENV ? JWT_SECRET : 'some-secret-key');
-//   } catch (err) {
-//     return next(new UnauthorizedError('Необходима авторизация'));
-//   }
-//   // записываем пейлоуд в объект запроса
-//   req.user = payload;
-
-//   // пропускаем запрос дальше
-//   return next();
-// };
-
-const handleAuthError = (next) => {
-  next(new UnauthorizedError('Необходима авторизация'));
-};
-
-const extractBearerToken = (header) => header.replace('Bearer ', '');
-
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  // извлекаем токен
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(next);
-  }
-
-  const token = extractBearerToken(authorization);
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV ? JWT_SECRET : 'secret-key');
+    // пытаемся верифицировать токен
+    payload = jwt.verify(token, NODE_ENV ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    return handleAuthError(next);
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
-
+  // записываем пейлоуд в объект запроса
   req.user = payload;
+
+  // пропускаем запрос дальше
   return next();
 };
